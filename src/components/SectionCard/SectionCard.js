205c46f19123f9b4ee41c7070ae1f4a0dcb69856
ms-yogi <bende.yogini@gmail.com>
2019-06-07 00:10:05 +0530

    Added onBlur event for small cards

--- src/components/SectionCard/SectionCard.js
+++ src/components/SectionCard/SectionCard.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTimes } from '@fortawesome/free-solid-svg-icons'
 import styles from './SectionCard.module.css'
 
 class SectionCard extends Component {
@@ -12,16 +14,37 @@ class SectionCard extends Component {
         })
     }
 
+    blurHandler = () => {
+        this.setState({
+            isDisabled : true
+        })
+    }
+
+    cardDeleteHandler = () => {
+        
+    }
+
     render() {
         return (
             <div className={["card", styles.card].join(' ')}>
+                <FontAwesomeIcon 
+                        icon={ faTimes } 
+                        className={styles.close}
+                        style={{color: '#ccc'}}
+                        onClick={this.cardDeleteHandler}/>
                 <div className={["card-body", styles.cardBody].join(' ')}
                 onClick={this.editHandler}>
                     <textarea
                         type="text" 
                         placeholder="Click to enter" 
                         className={styles.textarea}
-                        disabled={this.state.isDisabled}>
+                        disabled={this.state.isDisabled}
+                        onBlur={this.blurHandler}
+                        style={
+                            !this.state.isDisabled 
+                                ? { color : '#f2f2f2' } 
+                                : null 
+                        }>
                     </textarea>
                 </div>
             </div>
